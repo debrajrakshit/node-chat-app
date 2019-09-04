795453cf83c5ec2aedcc2c6008b091fba75f7bb7
Debraj Rakshit <debrajrakshit@Debrajs-MacBook-Air.local>
2019-09-04 13:20:54 +0530

    first commit

---
+++ public/js/chat.js
@@ -0,0 +1,126 @@
+const socket = io();
+
+// Eliments
+const $messageForm = document.querySelector('#message-form');
+const $messageFormInput = $messageForm.querySelector('input');
+const $messageFormButton = $messageForm.querySelector('button');
+const $sendLocationButton = document.querySelector('#send-location');
+const $messages = document.querySelector('#messages');
+
+// Templates
+const messageTemplate = document.querySelector('#message-template').innerHTML;
+const locationTemplate = document.querySelector('#location-template').innerHTML;
+const sidebarTemplate = document.querySelector('#sidebar-template').innerHTML;
+
+// Options
+const { username, room } = Qs.parse(location.search, { ignoreQueryPrefix: true });
+
+const autoScroll = () => {
+    // new message elemnt
+    const $newMessage = $messages.lastElementChild;
+
+    // height of the new message
+    const newMessageStyles = getComputedStyle($newMessage);
+    const newMessageMargin = parseInt(newMessageStyles.marginBottom);
+    const newMessageHeight = $newMessage.offsetHeight + newMessageMargin;
+
+    // visible height
+    const visibleHeight = $messages.offsetHeight;
+
+    // height of messages container
+    const containerHeight = $messages.scrollHeight;
+
+    // how far have I scrolled?
+    const scrollOffset = $messages.scrollTop + visibleHeight;
+
+    if (containerHeight - newMessageHeight <= scrollOffset) {
+        $messages.scrollTop = $messages.scrollHeight;
+    }
+
+}
+
+socket.on('message', (message) => {
+    console.log(message);
+    const html = Mustache.render(messageTemplate, {
+        username: message.username,
+        message: message.text,
+        createdAt: moment(message.createdAt).format('h:mm a')
+    });
+    $messages.insertAdjacentHTML('beforeend', html);
+    autoScroll();
+});
+
+socket.on('locationMessage', (message) => {
+    console.log(message);
+    const html = Mustache.render(locationTemplate, {
+        username: message.username,
+        url: message.url,
+        createdAt: moment(message.createdAt).format('h:mm a')
+    });
+    $messages.insertAdjacentHTML('beforeend', html);
+    autoScroll();
+});
+
+socket.on('roomData', ({ room, users }) => {
+    const html = Mustache.render(sidebarTemplate, {
+        room,
+        users
+    });
+
+    document.querySelector('#sidebar').innerHTML = html;
+});
+
+document.querySelector('#message-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    $messageFormButton.setAttribute('disabled', 'disabled');
+
+    // disable
+    const inputText = e.target.elements.message;
+    const message = inputText.value;
+
+    socket.emit('sendMessage', message, (error) => {
+        $messageFormButton.removeAttribute('disabled');
+        $messageFormInput.value = '';
+        $messageFormInput.focus();
+        // enable
+
+        if(error) {
+            return console.log(error);
+        }
+
+        console.log('Message delivered');
+    });
+});
+
+document.querySelector('#send-location').addEventListener('click', () => {
+    if(!navigator.geolocation){
+        return alert('Feolocation is not supported by your browser');
+    }
+
+    $sendLocationButton.setAttribute('disabled', 'disabled');
+
+    navigator.geolocation.getCurrentPosition((position) => {
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
+
+        socket.emit('sendLocation', {
+            latitude,
+            longitude
+        }, () => {
+            $sendLocationButton.removeAttribute('disabled');
+            console.log('Location shared');
+        });
+    });
+
+});
+
+socket.emit('join', { 
+    username: username, 
+    room: room 
+}, (error) => {
+    if(error) {
+        alert(error);
+        location.href = '/';
+    }
+});
---
+++ src/index.js
@@ -0,0 +1,6 @@
+const server = require('./app');
+const port = process.env.PORT;
+
+server.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
